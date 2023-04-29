7fc5def17bdf8c385c84b555485fac979e8db29d
nocturnalplay <nocturnalplay0@gmail.com>
2023-04-29 14:29:12 +0530

    updated

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,12 @@ const serverless = require("serverless-http");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require('cors');
 const app = express();
 const router = express.Router();
 
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
